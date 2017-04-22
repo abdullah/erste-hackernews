4649ddf686bbcbaacd335aff5bf4cd005d2786c0
Abdullah Mara <writer-edi@hotmail.com>
2017-04-22 21:17:15 +0300

    Add root view

---
+++ source/js/views/app.js
@@ -0,0 +1,11 @@
+import { View } from "erste";
+
+export default class App extends View {
+  template() {
+    return `
+    <root-view>
+        <h1>Hello Erste!</h1>
+    </root-view>
+    `;
+  }
+}
